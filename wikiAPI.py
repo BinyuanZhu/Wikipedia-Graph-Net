1e61c1482bf263ef79a9bc23118b1274013d8803
Daniel Zhu <danielb.zhu@mail.utoronto.ca>
2021-05-18 16:11:24 -0400

    Worked on main and fixed mockURL

--- wikiAPI.py
+++ wikiAPI.py
@@ -1,25 +1,32 @@
 """
 This file contains all the WikiMedia API request related functions. A sample call would look like:
-https://en.wikipedia.org/w/api.php?action=query&titles=Albert%20Einstein&prop=links
+https://en.wikipedia.org/w/api.php?action=query&titles=Albert%20Einstein&prop=links&pllimit=max
 for the query Albert Eintein
 """
 import requests
+
 mockTitle = "Dog"
-mockURL = "https://en.wikipedia.org/w/api.php?action=query&titles=Albert%20Einstein&prop=links&format=json"
+mockURL = "https://en.wikipedia.org/w/api.php?action=query&titles=Apple&prop=links&pllimit=max&format=json"
+
 
 def get_links(title: str) -> str:
-	"""
+    """
 	Given a query that can be assumed to be a Wikipedia Article Title, get the JSON for links back.
 	"""
-	baseURL = "https://en.wikipedia.org/w/api.php?action=query&titles=TEMP&prop=links&format=json"
-	query = title.replace(" ", "%20")
-	return baseURL.replace("TEMP", query)
+    baseURL = "https://en.wikipedia.org/w/api.php?action=query&titles=TEMP&prop=links&pllimit=max&format=json"
+    query = title.replace(" ", "%20")
+    return baseURL.replace("TEMP", query)
+
 
 def get_JSON(link: str) -> dict:
-	r = requests.get(link)
-	return r.json()
+    s = requests.Session()
+    r = s.get(link)
+    return r.json()
+
 
 def get_title(link):
-	pass
+    s = requests.Session()
+    r = s.get(link)
+    return r.json()
 
 # print(get_JSON(mockURL))
