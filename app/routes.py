bba7b1f6bd81bd942a37c6ad8cef0d6d740f4d20
EclipseIsDead <sidmcheath@gmail.com>
2021-05-19 16:54:40 -0400

    Added input form for start point

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,18 @@
-from flask import render_template
 from app import app
+import requests
+from flask import Flask, render_template, request
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', title='Home')
+    errors = []
+    results = {}
+    if request.method == "POST":
+        # get url that the user has entered
+        try:
+            start = request.form['start']
+            results.append(start)
+        except:
+            errors.append(
+                "Unable to get article. Please make sure it's valid and try again."
+            )
+    return render_template('index.html', errors=errors, results=results)
