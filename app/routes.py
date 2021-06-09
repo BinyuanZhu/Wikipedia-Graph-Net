a429cea4fd5787c04b9e89bcfae98d4afc57e6e7
EclipseIsDead <sidmcheath@gmail.com>
2021-06-09 17:22:38 -0400

    Allow users to specify heuristic

--- app/routes.py
+++ app/routes.py
@@ -4,13 +4,8 @@ from graph import a_star, heuristic_0, heuristic_1, heuristic_2
 from flask import Flask, render_template, request
 
 
-def test(start: str, end: str):
-    return [start, "1", "2", "3", end]
-
-
-def output(start: str, end: str) -> list:
-    path = a_star(start, end, heuristic_2)
-    return path
+def err():
+    return ["You are seeing this page because you did not choose a heuristic."]
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -23,7 +18,14 @@ def index():
         swap = True
         start = request.form['start']
         end = request.form['end']
-        data = test(start, end)
+        if request.form.get('action1') == 'BFS':
+            data = a_star(start, end, heuristic_0)
+        elif request.form.get('action2') == 'Intro Semantic':
+            data = a_star(start, end, heuristic_2)
+        elif request.form.get('action3') == 'Article Semantic':
+            data = a_star(start, end, heuristic_1)
+        else:
+            data = err()
         links = {title: get_url(title) for title in data}
         length = (len(data) - 2)
     if swap:
