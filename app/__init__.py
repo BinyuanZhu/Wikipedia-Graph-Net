7b667cbe6482ce4958f5fb5ea95b3164a72ef6ae
Baker Jackson <bakerwjackson@gmail.com>
2021-06-10 17:42:06 -0400

    Revert "Heroku functionality"
    This reverts commit 4487edc6145eca99fed0cbad66f83fea7baf3a03.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
-from flask_heroku import Heroku
 
 app = Flask(__name__)
-heroku = Heroku(app)
 
 from app import routes
--- main.py
+++ main.py
@@ -1,5 +1,2 @@
 from app import app
 import requests
-
-if __name__ == "__main__":
-    app.run(debug=False)
